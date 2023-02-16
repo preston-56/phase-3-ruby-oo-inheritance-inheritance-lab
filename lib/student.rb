a4d471f12523da504433204018e254e892532d91
Preston <prestonosoro56@gmail.com>
2023-02-16 18:05:26 +0300

    feat:student class inherits from User

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,17 @@
-class Student 
+require_relative "./user"
 
+class Student < User
+  attr_writer :knowledge
+
+  def initialize
+    @knowledge = []
+  end
+
+  def learn(some_knowledge)
+    @knowledge << some_knowledge
+  end
+
+  def knowledge
+    @knowledge
+  end
 end
